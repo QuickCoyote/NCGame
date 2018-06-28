c22f755ec3f9fa2efe925c89c6a24d329d00d804
QuickCoyote <36782326+QuickCoyote@users.noreply.github.com>
2018-06-28 13:21:32 -0600

    CurrentNCGame

---
+++ NCGame/Engine/engine.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <iostream>
+
+#include "sdl.h"
+
+#ifdef ENGINE_EXPORTS  
+#define ENGINE_API __declspec(dllexport)   
+#else  
+#define ENGINE_API __declspec(dllimport)
+#endif 
+
+class ENGINE_API Engine
+{
+public:
+	Engine() { }
+	~Engine() {}
+
+	bool Initialize();
+	void Update();
+	void Shutdown();
+	
+	bool IsQuit() { return m_isQuit; }
+
+	void Display() { std::cout << "Hello World!"; }
+
+protected:
+	bool m_isQuit = false;
+	SDL_Window * m_window = nullptr;
+	SDL_Renderer * m_renderer = nullptr;
+};
---
+++ NCGame/Project1/main.cpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "engine.h"
+
+int main(int argc, char* argv[])
+{
+	Engine engine;
+
+	engine.Initialize();
+	while (!engine.IsQuit())
+	{
+		engine.Update();
+	}
+	engine.Shutdown();
+
+	return 0;
+}
